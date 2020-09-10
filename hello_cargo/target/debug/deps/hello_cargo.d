49a377f4831276ed53308b719183127439bc2a5a
오유경 <ykoh@oyugyeong-ui-MacBookPro.local>
2020-09-10 16:39:19 +0900

    Hello world

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+/Users/ykoh/Desktop/Rust_study/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
+
+/Users/ykoh/Desktop/Rust_study/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/ykoh/Desktop/Rust_study/hello_cargo/target/debug/hello_cargo: /Users/ykoh/Desktop/Rust_study/hello_cargo/src/main.rs
