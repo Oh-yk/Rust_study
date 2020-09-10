ccacb7f0a55f40e2296b9e20373019632959167c
오유경 <gamtangerine@gmail.com>
2020-09-10 18:06:19 +0900

    variables

---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+/Users/ykoh/Desktop/Rust_study/variables/target/debug/deps/variables: src/main.rs
+
+/Users/ykoh/Desktop/Rust_study/variables/target/debug/deps/variables.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/ykoh/Desktop/Rust_study/variables/target/debug/variables: /Users/ykoh/Desktop/Rust_study/variables/src/main.rs
